af09bce0a73376e3d644909e569278e959cadb88
yassine2403 <86617346+yassine2403@users.noreply.github.com>
2021-07-16 17:58:46 +0100

    Add files via upload

---
+++ checkpoint10.py
@@ -0,0 +1,29 @@
+#ex1
+mylist=[14, "hello", 967]
+mylist[1]
+#ex2
+import pandas
+import numpy
+#ex3
+print("python errors")
+#ex4
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True]
+#ex5
+i=14
+while i<78:
+    print(i)
+    i+=1
+#ex6
+it=iter([1,2,3])
+next(it)
+next(it)
+next(it)
+#ex7
+'15'+'15'
+#ex8
+int('2345')
+#ex9
+a='python'
+#ex10
+x=19/0.1
